9f8837c548fc1730f6141c3365634df92b5cf1be
nomospace <jinlu_hz@163.com>
2013-01-17 14:41:03 +0800

    Update repo

---
+++ src/main/java/com/clip/web/utils/CoreConstants.java
@@ -0,0 +1,39 @@
+package com.clip.web.utils;
+
+public class CoreConstants {
+    public final static long ROLE_ASSOCIATION_TYPE_MENU = 1;
+    public final static long ROLE_ASSOCIATION_TYPE_NODE = 2;
+    public final static long ROLE_ASSOCIATION_TYPE_EXTENDED = 3;
+
+    public final static long ROLE_GLOBAL = 1;
+    public final static long ROLE_AVAILABLE = 1;
+    /**
+     * session中的用户名
+     */
+    public final static String USER_NAME = "username";
+    public final static String USER_CENTER = "usercenter";
+    public final static String USER_DEPARTMENT = "userdepartment";
+    public final static String USER_ROLE = "userrole";
+    public final static String USER_ROLE_STR = "userrolestr";
+    public final static String USER_ID = "userId";
+    public final static String USER_ROLE_CODE_ARRAY = "userrolecodearray";
+    public final static String USER_ROLE_Id_ARRAY = "userroleidarray";
+    public final static String USER_ROLE_NAME_ARRAY = "userrolenamearray";
+    public final static String USER_ROLE_NAMES_STR = "userrolenamesstr";
+    public final static String USER_ROLE_PRIVILEGE_BEAN = "userroleprivilegebean";
+    public final static String USER_UAM_GAME_ID = "useruamgameid";
+
+    /**
+     * session中的用户登录名
+     */
+    public final static String USER_LOGINNAME = "j_username";
+    public final static String USER_PASSWORD = "j_password";
+    /**
+     * session中的当前组织名
+     */
+    public final static String USER_SPID = "j_spId";
+    public static final String ACTION_KEY = "action";
+    public static final String NOT_AVAILABLE = "N/A";
+    public static final String ASSERTION = "assertion";
+
+}
