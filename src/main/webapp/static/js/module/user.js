60c2f29c687b637abc87fa0245d81e9fe632a01c
nomospace <jinlu_hz@163.com>
2013-03-20 01:09:27 +0800

    Refined token-util & updated repo

--- src/main/webapp/static/js/module/user.js
+++ src/main/webapp/static/js/module/user.js
@@ -1,5 +1,6 @@
 define(['masonry'], function () {
   'use strict';
+  $('html').removeAttr('contentEditable');
   $('#J_status_list').masonry({
     itemSelector: '.status-body'
 //    columnWidth: 240
